2f534a127f2c8a24a1c4db7947305a7187f65513
wangximing <wangximing@cryptape.com>
2021-12-03 11:53:34 +0800

    remove lumos in root address component

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { config } from "@ckb-lumos/lumos";
 import { Tabs } from "antd";
 import "antd/dist/antd.css";
 import styled from "styled-components";
@@ -7,7 +6,6 @@ const { TabPane } = Tabs;
 import { AddressToScript } from "./AddressToScript";
 import { ScriptToAddress } from "./ScriptToAddress";
 
-config.initializeConfig(config.predefined.AGGRON4);
 const StyleWrapper = styled.div`
   padding: 20px;
 `;
